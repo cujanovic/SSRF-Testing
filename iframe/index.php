971359ccf938c547ac6a99a0c182541150d560e2
Predrag Cujanovic <predrag@cujanovic.com>
2021-09-18 19:40:11 +0200

    update

--- iframe/index.php
+++ iframe/index.php
@@ -4,10 +4,14 @@ header("Content-Type: text/html");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Pragma: no-cache");
 
-echo '<html><body>';
-echo '<iframe src="'.$_GET["proto"].'://google.com:80+&@'.$_GET["ip"].':'.$_GET["port"].$_GET["url"].'#+@google.com:80/" height="180px" width="80%"></iframe>';
-echo '<iframe src="'.$_GET["proto"].'://'.$_GET["ip"].':'.$_GET["port"].$_GET["url"].'+&@google.com:80#+@google.com:80/" height="180px" width="80%"></iframe>';
-echo '<iframe src="'.$_GET["proto"].'://google.com:80+&@google.com:80#+@'.$_GET["ip"].':'.$_GET["port"].$_GET["url"].'" height="180px" width="80%"></iframe>';
-echo '<iframe src="'.$_GET["proto"].'://'.$_GET["ip"].':'.$_GET["port"].$_GET["url"].'?@google.com:80/" height="180px" width="80%"></iframe>';
-echo '<iframe src="'.$_GET["proto"].'://'.$_GET["ip"].':'.$_GET["port"].$_GET["url"].'#@www.google.com:80/" height="180px" width="80%"></iframe>';
-echo '</body></html>';
+if (filter_var($_GET["ip"], FILTER_VALIDATE_IP) && filter_var($_GET["port"], FILTER_VALIDATE_INT) && isset($_GET["proto"]) && isset($_GET["url"])){
+    $proto = urlencode($_GET["proto"]);
+    $url = htmlentities(($_GET["url"]), ENT_COMPAT, 'UTF-8');
+    echo '<html><body>';
+    echo '<iframe src="'.$proto.'://google.com:80+&@'.$_GET["ip"].':'.$_GET["port"].$url.'#+@google.com:80/" height="180px" width="80%"></iframe>';
+    echo '<iframe src="'.$proto.'://'.$_GET["ip"].':'.$_GET["port"].$url.'+&@google.com:80#+@google.com:80/" height="180px" width="80%"></iframe>';
+    echo '<iframe src="'.$proto.'://google.com:80+&@google.com:80#+@'.$_GET["ip"].':'.$_GET["port"].$url.'" height="180px" width="80%"></iframe>';
+    echo '<iframe src="'.$proto.'://'.$_GET["ip"].':'.$_GET["port"].$url.'?@google.com:80/" height="180px" width="80%"></iframe>';
+    echo '<iframe src="'.$proto.'://'.$_GET["ip"].':'.$_GET["port"].$url.'#@www.google.com:80/" height="180px" width="80%"></iframe>';
+    echo '</body></html>';
+}
